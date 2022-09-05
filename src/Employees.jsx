a27b0cfefbda69169b19af04f37604a8c9ed18bd
aleksandergjinollari <aleksandergjinollari@hotmail.com>
2022-09-05 11:15:28 +0200

    added localStorage to persist our teams

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -21,7 +21,7 @@ const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeam
             {
               employees.map((employee) => (
                 <div id={employee.id} className={(employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2")} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
-                  {(employee.gender === "male") ?<img src={maleProfile} classname="card-img-top" /> : <img src={femaleProfile} classname="card-img-top" />}
+                  {(employee.gender === "male") ?<img src={maleProfile} alt="male profile" classname="card-img-top" /> : <img src={femaleProfile} alt="female profile" classname="card-img-top" />}
                   <div className="card-body">
                     <h5 className="card-title">Full Name: {employee.fullName}</h5>
                     <p className="card-text"><b>Designation:</b> {employee.designation}</p>
